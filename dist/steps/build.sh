d8d95dfb5e9f92121a98b3686dc71f9ac883761a
danyow <i.zuucol@gmail.com>
2021-11-25 19:59:16 +0800

    zip

--- dist/steps/build.sh
+++ dist/steps/build.sh
@@ -161,6 +161,7 @@ echo ""
 
 GRADLE_VERSION="6.7.1"
 wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip -P /tmp
+apt-get install zip
 unzip -d /opt/gradle /tmp/gradle-${GRADLE_VERSION}-bin.zip
 ln -s /opt/gradle/gradle-${GRADLE_VERSION} /opt/gradle/latest
 cat>/etc/profile.d/gradle.sh<<EOF
